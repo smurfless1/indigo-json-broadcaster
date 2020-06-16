77344369c121e1c0f62d644b8e55d6b19511d77f
David Brown <davidb@netflix.com>
2020-06-15 23:39:16 -0500

    influx 2 changes

--- JSONbroadcast.indigoPlugin/Contents/Server Plugin/indigo_adaptor.py
+++ JSONbroadcast.indigoPlugin/Contents/Server Plugin/indigo_adaptor.py
@@ -4,24 +4,27 @@ import json
 import indigo
 from enum import Enum
 
+
 # explicit changes
 def indigo_json_serial(obj):
     """JSON serializer for objects not serializable by default json code"""
-    #indigo.server.log(unicode(obj))
+    # indigo.server.log(unicode(obj))
     if isinstance(obj, (datetime, date)):
         ut = time_.mktime(obj.timetuple())
         return int(ut)
     if isinstance(obj, indigo.Dict):
         dd = {}
-        for key,value in obj.iteritems():
+        for key, value in obj.iteritems():
             dd[key] = value
         return dd
-    raise TypeError ("Type %s not serializable" % type(obj))
+    raise TypeError("Type %s not serializable" % type(obj))
+
 
 class IndigoAdaptor():
     '''
     Change indigo objects to flat dicts for simpler databases
     '''
+
     def __init__(self):
         self.debug = False
         # Class Properties on http://wiki.indigodomo.com/doku.php?id=indigo_7_documentation:device_class
@@ -40,11 +43,11 @@ class IndigoAdaptor():
     def smart_value(self, invalue, mknumbers=False):
         value = None
         if unicode(invalue) != u"null" \
-            and unicode(invalue) != u"None" \
-            and not isinstance(invalue, indigo.List) \
-            and not isinstance(invalue, list) \
-            and not isinstance(invalue, indigo.Dict) \
-            and not isinstance(invalue, dict):
+                and unicode(invalue) != u"None" \
+                and not isinstance(invalue, indigo.List) \
+                and not isinstance(invalue, list) \
+                and not isinstance(invalue, indigo.Dict) \
+                and not isinstance(invalue, dict):
             value = invalue
             try:
                 if mknumbers:
@@ -68,7 +71,7 @@ class IndigoAdaptor():
                 # convert datetime to timestamps of another flavor
                 elif isinstance(invalue, (datetime, date)):
                     value = time_.mktime(invalue.timetuple())
-                    #value = int(ut)
+                    # value = int(ut)
                 # explicitly change enum values to strings
                 # TODO find a more reliable way to change enums to strings
                 elif invalue.__class__.__bases__[0].__name__ == 'enum':
@@ -83,27 +86,27 @@ class IndigoAdaptor():
     def to_json(self, device):
         attrlist = [attr for attr in dir(device) if
                     attr[:2] + attr[-2:] != '____' and not callable(getattr(device, attr))]
-        #indigo.server.log(device.name + ' ' + ' '.join(attrlist))
+        # indigo.server.log(device.name + ' ' + ' '.join(attrlist))
         newjson = {}
         newjson[u'name'] = unicode(device.name)
         for key in attrlist:
-            #import pdb; pdb.set_trace()
+            # import pdb; pdb.set_trace()
             if hasattr(device, key) \
-                and key not in newjson.keys():
-                val = self.smart_value(getattr(device, key), False);
+                    and key not in newjson.keys():
+                val = self.smart_value(getattr(device, key), False)
                 # some things change types - define the original name as original type, key.num as numeric
                 if val != None:
                     newjson[key] = val
                 if key in self.stringonly:
                     continue
-                val = self.smart_value(getattr(device, key), True);
+                val = self.smart_value(getattr(device, key), True)
                 if val is not None:
                     newjson[key + '.num'] = val
 
         # trouble areas
         # dicts end enums will not upload without a little abuse
         for key in 'states globalProps pluginProps ' \
-            'ownerProps'.split():
+                   'ownerProps'.split():
             if key in newjson.keys():
                 del newjson[key]
 
@@ -116,14 +119,14 @@ class IndigoAdaptor():
                 newjson[key] = unicode(newjson[key])
 
         for state in device.states:
-            val = self.smart_value(device.states[state], False);
-            if val != None:
-                newjson[unicode('state.' + state)] = val
+            val = self.smart_value(device.states[state], False)
+            if val is not None:
+                newjson[unicode('state_' + state)] = val
             if state in self.stringonly:
                 continue
-            val = self.smart_value(device.states[state], True);
-            if val != None:
-                newjson[unicode('state.' + state + '.num')] = val
+            val = self.smart_value(device.states[state], True)
+            if val is not None:
+                newjson[unicode('state_' + state + '_num')] = val
 
         # Try to tell the caller what kind of measurement this is
         if u'setpointHeat' in device.states.keys():
@@ -137,11 +140,18 @@ class IndigoAdaptor():
         for key in self.typecache.keys():
             if key in newjson.keys():
                 try:
-                    newjson[key] = eval( '%s("%s")' % (self.typecache[key], str(newjson[key])))
+                    newjson[key] = eval('%s("%s")' % (self.typecache[key], str(newjson[key])))
                 except ValueError:
                     if self.debug:
                         indigo.server.log('One of the columns just will not convert to the requested type. Partial record written.')
-                    pass
+
+        # periods as key delimiters don't travel well
+        for key in newjson.keys():
+            key: str
+            newkey = key.replace(".", "_")
+            if key != newkey:
+                newjson[newkey] = newjson[key]
+                del newjson[key]
 
         return newjson
 
@@ -149,6 +159,9 @@ class IndigoAdaptor():
         # strip out matching values?
         # find or create our cache dict
         newjson = self.to_json(device)
+        if self.debug:
+            indigo.server.log(json.dumps(newjson, default=indigo_json_serial).encode('utf-8'))
+        return newjson
 
         localcache = {}
         if device.name in self.cache.keys():
@@ -156,11 +169,10 @@ class IndigoAdaptor():
 
         diffjson = {}
         for kk, vv in newjson.iteritems():
-            if kk not in localcache or localcache[kk] != vv:
-                if not isinstance(vv, indigo.Dict) and not isinstance(vv, dict):
-                    diffjson[kk] = vv
+            if kk not in localcache or localcache[kk] != vv and not isinstance(vv, indigo.Dict) and not isinstance(vv, dict):
+                diffjson[kk] = vv
 
-        if not device.name in self.cache.keys():
+        if device.name not in self.cache.keys():
             self.cache[device.name] = {}
         self.cache[device.name].update(newjson)
 
@@ -175,4 +187,3 @@ class IndigoAdaptor():
             indigo.server.log(json.dumps(diffjson, default=indigo_json_serial).encode('utf-8'))
 
         return diffjson
-
